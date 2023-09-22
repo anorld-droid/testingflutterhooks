226ca3a9012897ff04e64d1e5c844d447c2be89c
anorld-droid <mulindipatrice00@gmail.com>
2023-09-22 08:58:41 +0300

    Init flutter hooks

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
+    ),
+  );
+}
+
+Stream<String> getTime() => Stream.periodic(
+      const Duration(seconds: 1),
+      (_) => DateTime.now().toIso8601String(),
+    );
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final dateTime = useStream(getTime());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(dateTime.data ?? widget.title),
+      ),
+    );
+  }
+}
