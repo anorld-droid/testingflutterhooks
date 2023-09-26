8fe2fd7654b6de87fe6d84875352b7296d3f0dfd
anorld-droid <mulindipatrice00@gmail.com>
2023-09-26 05:49:04 +0300

    Created a countdown with useMemoized and useListenable.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 // Reduce an iterable to values removing null if a transform func is not passed!!
@@ -27,13 +28,54 @@ Stream<String> getTime() => Stream.periodic(
       (_) => DateTime.now().toIso8601String(),
     );
 
-const url = 'https://bit.ly/3qYOtDm';
+// const url = 'https://bit.ly/3qYOtDm';
+
+class Countdown extends ValueNotifier<int> {
+  late StreamSubscription sub;
+  Countdown({required int from}) : super(from) {
+    sub = Stream.periodic(const Duration(seconds: 1), (v) => from - v)
+        .takeWhile((value) => value >= 0)
+        .listen((value) {
+      this.value = value;
+    });
+  }
+
+  @override
+  void dispose() {
+    sub.cancel();
+    super.dispose();
+  }
+}
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final countdown = useMemoized(() => Countdown(from: 20));
+    final notifier = useListenable(countdown);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Home Page'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            notifier.value.toString(),
+            style: const TextStyle(fontSize: 32),
+          )
+        ],
+      ),
+    );
+    // body: Column(children: [
+    //   TextField(
+    //     controller: controller,
+    //   ),
+    //   Text('New Typed: ${text.value}'),
+    // ]),
+    // );
     // final dateTime = useStream(getTime());
     // final controller = useTextEditingController();
     // final text = useState('');
@@ -47,25 +89,10 @@ class HomePage extends HookWidget {
     //   [controller],
     // );
     // To avoid the flicker where useFuture continously loads data, we use useMemoized for caching
-    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(url))
-        .load(url)
-        .then((data) => data.buffer.asUint8List())
-        .then((data) => Image.memory(data)));
-    final snapshot = useFuture(future);
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Home Page'),
-      ),
-      body: Column(
-        children: [snapshot.data].compactMap().toList(),
-      ),
-      // body: Column(children: [
-      //   TextField(
-      //     controller: controller,
-      //   ),
-      //   Text('New Typed: ${text.value}'),
-      // ]),
-    );
+    // final future = useMemoized(() => NetworkAssetBundle(Uri.parse(url))
+    //     .load(url)
+    //     .then((data) => data.buffer.asUint8List())
+    //     .then((data) => Image.memory(data)));
+    // final snapshot = useFuture(future);
   }
 }
